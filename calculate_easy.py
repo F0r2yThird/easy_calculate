0098c420bd427da411ad0ac08195366368e4a9e1
F0r2yThird <46202199+F0r2yThird@users.noreply.github.com>
2020-11-12 10:25:47 +0300

    Add files via upload

---
+++ calculate_easy.py
@@ -0,0 +1,45 @@
+def welcome():
+    print("Добро пожаловать в калькулятор ")
+
+def calculate():
+
+    a = float(input("Ввидите значение 1: "))
+    b = float(input("Ввидите значение 2: "))
+
+    what = input("+/-/*/**/ / /%/ : ")
+
+    if what == "+":
+        c = a + b
+        print("Результат: " + str(c))
+    elif what == "-":
+        c = a - b
+        print("Результат: " + str(c))
+    elif what == "*":
+        c = a * b
+        print("Результат: " + str(c))
+    elif what == "**":
+        c = a ** b
+        print("Результат: " + str(c))
+    elif what == "/":
+        c = a / b
+        print("Результат: " + str(c))
+    elif what == "%":
+        c = a % b
+        print("Результат: " + str(c))
+
+    else:
+        print("Выбрана не верная операция!")
+
+def again():
+    calc_again = input("Хочешь выйти напиши N, если остаться Y: ")
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print("Увидимся ещё!")
+    else:
+        again()
+
+welcome()
+calculate()
+again()
